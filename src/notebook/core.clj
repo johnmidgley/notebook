3628510e4501333818dd9089f13bb792670a7e15
John Midgley <john.midgley@gmail.com>
2016-08-15 17:35:04 -0700

    Added basic note handling

--- src/notebook/core.clj
+++ src/notebook/core.clj
@@ -1,3 +1,9 @@
 (ns notebook.core)
 
+(def notes (range 0 128))
+(def labels [:C :C#/Db :D :D#/Eb :E :F :F#/Gb :G :G#/Ab :A :A#/Bb :B])
+(def notes-in-octave (count labels))
+
+(defn note->label [note]
+  (labels (mod note notes-in-octave)))
 
