d7bbf2071d679dd8e6c124aa080fbc54fe460c6d
John Midgley <john.midgley@gmail.com>
2016-08-27 21:56:12 -0400

    Reworked IntelliJ and got basic notes working

--- src/notebook/core.clj
+++ src/notebook/core.clj
@@ -1,9 +1,35 @@
-(ns notebook.core)
+(ns notebook.core
+  (:require [clojure.math.numeric-tower :as math]))
 
-(def notes (range 0 128))
-(def labels [:C :C#/Db :D :D#/Eb :E :F :F#/Gb :G :G#/Ab :A :A#/Bb :B])
-(def notes-in-octave (count labels))
+(def midi-nums (range 0 128))
+(def pitch-classes [[:C] [:C# :Db] [:D] [:D# :Eb] [:E] [:F]
+                    [:F# :Gb]
+                    [:G] [:G# :Ab] [:A] [:A# :Bb] [:B]])
+(def notes-in-octave (count pitch-classes))
 
-(defn note->label [note]
-  (labels (mod note notes-in-octave)))
+(defn midi-num->pitch-class [midi-num]
+  (-> (mod midi-num notes-in-octave)
+      pitch-classes
+      first))
+
+(defn note->octave [midi-num]
+  (-> midi-num
+      (quot notes-in-octave)
+      dec))
+
+(defn midi-num->frequency [midi-num]
+  (* (math/expt 2 (/ (- midi-num 69) 12)) 440))
+
+(defn midi-num->note [midi-num]
+  (let [pitch-class (midi-num->pitch-class midi-num)
+        octave (note->octave midi-num)]
+    {:midi-num midi-num
+     :frequency (midi-num->frequency midi-num)
+     :pitch-class pitch-class
+     :octave octave
+     :name (str (name pitch-class) octave)}))
+
+(def notes (mapv midi-num->note midi-nums))
+
+
 
