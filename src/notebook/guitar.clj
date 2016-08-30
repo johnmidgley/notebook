b351540b1753de80755ed44172da170d22676e81
John Midgley <john.midgley@gmail.com>
2016-08-30 17:20:06 -0400

    Increased fretboard

--- src/notebook/guitar.clj
+++ src/notebook/guitar.clj
@@ -15,4 +15,4 @@
 (def open-notes-standard-six-string ["E4" "D3" "G3" "D3" "A2" "E2"])
 
 (def standard-six-string-guitar
-  (guitar (map core/name->note open-notes-standard-six-string) 10))
+  (guitar (map core/name->note open-notes-standard-six-string) 20))
