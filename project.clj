8d2135c834025dc3fc76932fa85916dd1fad62a0
John Midgley <john.midgley@gmail.com>
2016-08-30 18:01:24 -0400

    Added midje testing

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]
+                 ; Should be in dev dependencies
+                 [midje "1.8.3"]])
