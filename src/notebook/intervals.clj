8c5ee63ff73210f164bd6353f17f05f275716c71
John Midgley <john.midgley@gmail.com>
2017-01-04 23:40:52 -0800

    Added intervals

---
+++ src/notebook/intervals.clj
@@ -0,0 +1,33 @@
+(ns notebook.intervals)
+
+(def P1 0)                                                  ; Perfect unison
+(def d2 0)                                                  ; Diminished second
+(def m2 1)                                                  ; Minor second
+(def A1 1)                                                  ; Augmented unison
+(def S 1)                                                   ; Semitone
+(def M2 2)                                                  ; Major second
+(def d3 2)                                                  ; Diminished third
+(def T 2)                                                   ; Tone
+(def m3 3)                                                  ; Minor third
+(def A2 3)                                                  ; Augmented second
+(def M3 4)                                                  ; Major third
+(def d4 4)                                                  ; Diminished fourth
+(def P4 5)                                                  ; Perfect fourth
+(def A3 5)                                                  ; Augmented third
+(def d5 6)                                                  ; Diminished fifth
+(def A4 6)                                                  ; Augmented fourth
+(def TT 6)                                                  ; Tritone
+(def P5 7)                                                  ; Perfect fifth
+(def d6 7)                                                  ; Diminished sixth
+(def m6 8)                                                  ; Minor sixth
+(def A5 8)                                                  ; Augmented fifth
+(def M6 9)                                                  ; Major sixth
+(def d7 9)                                                  ; Diminished seventh
+(def m7 10)                                                 ; Minor seventh
+(def A6 10)                                                 ; Augmented sixth
+(def M7 11)                                                 ; Major seventh
+(def d8 11)                                                 ; Diminished octave
+(def P8 12)                                                 ; Perfect octave
+(def A7 12)                                                 ; Augmented seventh
+
+;; Add compound intervals from https://en.wikipedia.org/wiki/Interval_(music)#Compound_intervals
